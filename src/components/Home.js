05f0b6afac318d0986ec86965da0a7f4335b674e
Harsh <73820704+enthoharsh@users.noreply.github.com>
2025-04-09 16:39:06 +0530

    user instruction

--- src/components/Home.js
+++ src/components/Home.js
@@ -81,10 +81,10 @@ export default function Home() {
         darkMode ? "dark bg-gray-900 text-white" : "bg-blue-50 text-black"
       }
     >
-      <div className="mb-6 p-4 rounded-xl bg-white/5 backdrop-blur-md border border-white/10 text-center text-sm text-black shadow-md">
+      <div className="mb-6 p-4 rounded-xl bg-white/5 backdrop-blur-md border border-white/10 text-center text-sm  shadow-md">
   📂 <strong>Please upload your exported WhatsApp chat (.txt file)</strong><br />
   To use this tool, first export your chat from WhatsApp and upload the .txt file here.<br />
-  <span className="text-xs text-gray-300">Go to WhatsApp {'>'} Chat {'>'} Export Chat {'>'} Without Media {'>'} Save as .txt file</span>
+  <span className="text-xs ">Go to WhatsApp {'>'} Chat {'>'} Export Chat {'>'} Without Media {'>'} Save as .txt file</span>
 </div>
 
       <div className="min-h-screen flex flex-col px-4 py-6 max-w-5xl mx-auto">
